5f6554b6bb6ba561b79e6bfaa0c568012040a062
Renato Alexandre <renatoacf@gmail.com>
2021-04-10 21:32:41 -0300

    Commit da primeira versão

--- exemplo.php
+++ exemplo.php
@@ -6,6 +6,6 @@ use RenatoAlexandre\ProjetoCEP\Search;
 
 $busca = new Search;
 
-$resultado = $busca->getAddressFromZipcode('62800000');
+$resultado = $busca->getAddressFromZipcode('62960000');
 
 print_r($resultado);
