de11d673a0e572834226a9d04ee30238362cd5d7
Renato Alexandre <renatoacf@gmail.com>
2021-04-10 21:39:23 -0300

    Gerando primeira release

--- exemplo.php
+++ exemplo.php
@@ -6,6 +6,6 @@ use RenatoAlexandre\ProjetoCEP\Search;
 
 $busca = new Search;
 
-$resultado = $busca->getAddressFromZipcode('62960000');
+$resultado = $busca->getAddressFromZipcode('62930000');
 
 print_r($resultado);
